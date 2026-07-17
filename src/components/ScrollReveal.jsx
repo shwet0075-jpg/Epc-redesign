bc01cfe047d477be47c545eb295ab3e9b4b01303
shwet0075-jpg <shwet0075@gmail.com>
2026-07-17 20:36:27 +0530

    Update

--- src/components/ScrollReveal.jsx
+++ src/components/ScrollReveal.jsx
@@ -4,7 +4,7 @@ export default function ScrollReveal({
   children,
   variant = 'fade-up',
   delay = 0,
-  duration = 0.5,
+  duration = 0.48,
   className = '',
   viewportOnce = true,
   amount = 0.15
